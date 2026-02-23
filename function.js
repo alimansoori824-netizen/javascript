04480e87e619e882b341ebeb99b3cdcb61792bb9
Ali Mansoori <alimansoori824@gmail.com>
2026-02-24 00:02:23 +0530

    functionjs

---
+++ function.js
@@ -0,0 +1,21 @@
+// let stu = ["ali","ummarried",19,"55kg","Kolata"]
+// console.log(stu);
+// let ar9 = stu.slice(1,4)
+// console.log(ar9);
+//slice 2nd bracket starts from 1 indexing instead of 0
+
+//splice - delete and add elements(will read also)
+// for adding - 2 arguements
+// for deleting - 3 arguements
+
+// let stu = ["ali","ummarried",19,"55kg","Kolata"];
+// console.log(stu);
+// // let ar2= stu.splice(2,1);//for deleting
+// // console.log(ar2);
+
+// let ar3= stu.splice(2,0,"orange");//for adding (0 for not deleting anything)
+// console.log(ar3);
+
+//23 feb
+
+
