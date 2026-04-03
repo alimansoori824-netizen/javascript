0ea49e6e3f03b3a751726d370c2df25dcc9046db
Ali Mansoori <alimansoori824@gmail.com>
2026-04-03 21:49:28 +0530

    add sun mul div js

---
+++ scriptt.js
@@ -0,0 +1,16 @@
+import sum from './sum.js'
+import sub from './sub.js'
+import {mul as m} from './mul.js'
+import {div as d} from './div.js'
+// let add= sum()
+// console.log("addition is : ",add)
+
+// let dec = sub()
+// console.log("subtraction is : ",dec)
+
+// let mull = m()
+// console.log("multiplication is : ",mull)
+
+let divv = d()
+console.log("division is : ",divv)
+
