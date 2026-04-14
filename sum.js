739f793bb58cad4b6c262260fd451358d447a712
Ali Mansoori <alimansoori824@gmail.com>
2026-04-14 22:56:44 +0530

    addition js

---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    // with arguement and with return
+    a=parseInt(prompt("Enter 1st number "))
+    b=parseInt(prompt("Enter 2nd number "))
+    return a+b 
+    
+}
+export default sum
